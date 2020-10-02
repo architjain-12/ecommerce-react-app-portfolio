c81ff799dcbe42beb74117eb4666d5edc90366d8
architjain-12 <architjain08@gmail.com>
2020-10-02 21:53:51 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import {BrowserRouter , Switch , Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-const HatsPage = () => (
-  <div>
-      <h1>HATSPAGE</h1>
-  </div>
-);
+import Header from './components/header/header.component.jsx';
+
+// const HatsPage = () => (
+//   <div>
+//       <h1>HATSPAGE</h1>
+//   </div>
+// );
 
 
 function App() {
   return (
     <div>
+    
     <BrowserRouter>
+    <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
